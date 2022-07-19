50e7fced6be3851c85aa873f4715e2d0577d7163
Luke D'Souza <lukedsouza99@outlook.com>
2022-07-19 23:29:31 +0100

    OOP

--- EloMaths.py
+++ EloMaths.py
@@ -2,30 +2,25 @@
 class EloMaths():
 
 
-    def __init__(self, dividor, kFactor):
-        self.dividor = dividor 
+    dividor = 400
+
+    def __init__(self, kFactor):
         self.kFactor = kFactor
 
     
     # Calculates probability of winning when given two ELOs.
     def probabilityOfWin(self, eloA, eloB):
-        
         probabilityOfA = 1 / (1 + 10**((eloB - eloA)/self.dividor))
         return [probabilityOfA, 1 - probabilityOfA]
 
 
     # Updates ELOs using probability of winning and actual results.
     def updateScoreAssumeAWin(self,eloA, eloB, wins):
-
         probabilities = self.probabilityOfWin(eloA, eloB)
-        print(probabilities)
+        updatedEloA = eloA + self.kFactor * (wins[0] - probabilities[0])
+        updatedEloB = eloB + self.kFactor * (wins[1] - probabilities[1])
         
-
-
-x = EloMaths(400,16)
-
-x.updateScoreAssumeAWin(2600,2300,1)
-
+        
 
 
 
