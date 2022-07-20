d7ef2aed493c01092ed065eb4ba04e1dc6046256
Luke D'Souza <lukedsouza99@outlook.com>
2022-07-20 01:00:22 +0100

    tired

--- ComicAccess.py
+++ ComicAccess.py
@@ -5,8 +5,22 @@ class ComicAccess():
 
     csv = pd.read_csv(r'comic_rating.csv')
 
+    def saveCsv(self):
+        self.csv.to_csv(r'comic_rating.csv', header = True, index = False)
+
     def getRandomComic(self):
         random_comic = self.csv.iloc[random.randint(0,self.csv.shape[0]-1)]
         return random_comic
 
+
+    def updateElo(self, index, elo):
+        self.csv.loc[index,"Score"] = elo
+        self.saveCsv()
+    
+
+    def addNew(self,title,writer,artist,year,issues,score):
+        new_row =pd.DataFrame({"Title":[title],"Writer":[writer],"Artist":[artist],"Year":[year],"Issues":[issues],"Score":[score]})
+        self.csv = pd.concat([self.csv, new_row], axis = 0, ignore_index = True)
+        print(self.csv)
+        self.saveCsv()
 
--- main.py
+++ main.py
@@ -7,5 +7,7 @@ eloMaths = EM.EloMaths(kFactor)
 comicAccess = ComicAccess.ComicAccess()
 print(comicAccess.getRandomComic())
 
+comicAccess.updateElo(0,20)
 
+comicAccess.addNew("Hawkeye", "Matt Fraction", "David Aja", 2017, 50, 30)
 
